c9e1746d002427f66e4c67f72179d82881042606
Jon Butler <Jwb2472@gmail.com>
2020-10-19 17:27:18 -0400

    Updates

---
+++ precache-manifest.e42d30ee925f27b0c9251848de5f5660.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9137e173960abc3a60c4d4d7637c8e4",
+    "url": "/list/index.html"
+  },
+  {
+    "revision": "5ed282fffb0c651c4479",
+    "url": "/list/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "646a10829bcd14bdb696",
+    "url": "/list/static/js/2.ffa27d2b.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/list/static/js/2.ffa27d2b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ed282fffb0c651c4479",
+    "url": "/list/static/js/main.eb9f828f.chunk.js"
+  },
+  {
+    "revision": "f42969d5aa6a836dc9c9",
+    "url": "/list/static/js/runtime-main.b8bd1ffa.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/list/static/media/logo.5d5d9eef.svg"
+  }
+]);
